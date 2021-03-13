2106fe26907ddc854a3abc46f198a23dbb17eed7
Hendrick <sebastinesoacatp@gmail.com>
2021-03-13 21:07:07 +0100

    Done

--- app/src/main/java/com/hendrick/framework/mvvm/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/hendrick/framework/mvvm/ui/main/view/MainActivity.kt
@@ -1,12 +1,78 @@
 package com.hendrick.framework.mvvm.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.hendrick.framework.mvvm.R
+import com.hendrick.framework.mvvm.data.api.ApiHelper
+import com.hendrick.framework.mvvm.data.api.ApiServiceImpl
+import com.hendrick.framework.mvvm.data.model.User
+import com.hendrick.framework.mvvm.ui.base.ViewModelFactory
+import com.hendrick.framework.mvvm.ui.main.adapter.MainAdapter
+import com.hendrick.framework.mvvm.ui.main.viewmodel.MainViewModel
+import com.hendrick.framework.mvvm.utils.Status
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var adapter: MainAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupUI()
+        setupViewModel()
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        mainViewModel.getUsers().observe(this, Observer {
+            when( it.status){
+                Status.SUCCESS -> {
+                    progressBar.visibility = View.GONE
+                    it.data?.let { users -> renderList(users) }
+                    recyclerView.visibility = View.VISIBLE
+                }
+                Status.LOADING -> {
+                    progressBar.visibility = View.VISIBLE
+                    recyclerView.visibility = View.GONE
+                }
+                Status.ERROR -> {
+                    // Handle Error
+                    progressBar.visibility = View.GONE
+                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
+
+    private fun renderList(users: List<User>){
+        adapter.addData(users)
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun setupViewModel() {
+        mainViewModel = ViewModelProviders.of(
+            this,
+            ViewModelFactory(ApiHelper(ApiServiceImpl()))
+        ).get(MainViewModel::class.java)
+    }
+
+    private fun setupUI() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MainAdapter(arrayListOf())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
     }
 }
